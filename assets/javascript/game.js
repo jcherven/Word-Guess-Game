894f9d43976e5ef18be5a6e2e9c1813a1fe63888
James Cherven <jcherven@mail.usf.edu>
2018-09-02 16:37:35 -0400

    Adds handling for repeated incorrect guesses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,28 +1,32 @@
 // GLOBALS
 // Create the dicitonary
-var dictionary = ['hamburger', 'cheeseburger', 'fries', 'milkshake', 'soda', 'knife'];
+const dictionary = [
+  'hamburger', 'cheeseburger', 'fries', 'taco', 'nuggets',
+  'doughnut', 'coffee', 'milkshake', 'soda', 'knife'
+];
+
 // Create a letter bank of the alphabet to check valid guesses
-var letterBank = [
+const letterBank = [
   'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
   'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
-]
+];
 
 // Choose a word from the dictionary (random)
 var randomWordPicker = Math.floor(Math.random() * dictionary.length);
-// Turn the chosen word into an array
-var activeWord = dictionary[randomWordPicker].split('');
+// Turn the chosen word into an array of lowercase letters
+var activeWord = dictionary[randomWordPicker].toLowerCase().split('');
 console.log('ACTIVE WORD IS: ' + activeWord)
-var underscores = [];
-var correctLetters = [];
-var incorrectLetters = [];
+var blankLetters = [];
+var correctGuesses = [];
+var incorrectGuesses = [];
 
 // Display empty letters in #current-word-area from the length of the chosen word
 var emptyLetters = function() {
   for (i = 0; i < activeWord.length; i++) {
-    underscores.push('_');
-    document.getElementById('current-word-area').innerHTML = underscores.join(' ');
+    blankLetters.push('_');
+    document.getElementById('current-word-area').innerHTML = blankLetters.join(' ');
   };
-  return underscores;
+  return blankLetters;
 }; 
 
 console.log(activeWord);
@@ -35,17 +39,30 @@ document.addEventListener('keypress', function(event) {
   var letter = String.fromCharCode(event.keyCode).toLowerCase();
 
   // Check that the player's guess exists in letterBank
+  if (letterBank.indexOf(letter) > -1) {
+    // If guess is incorrect, insert in incorrectGuesses array
+    if (activeWord.indexOf(letter) === -1) {
+      // If letter is not already in the incorrectGuesses, push it in
+      if (incorrectGuesses.indexOf(letter) === -1) {
+        incorrectGuesses.push(letter);
+        incorrectGuesses.sort();
+        document.getElementById("incorrect-guesses").innerHTML = incorrectGuesses.join(' ');
+      }
+      else {
+        console.log(letter + " has already been guessed");
+      }
+    }
+    // If guess is correct, insert in correctGuesses array
+    else if (activeWord.indexOf(letter) > -1) {
+      correctGuesses.splice(activeWord.indexOf(letter), 0, letter);
+      console.log('correct: ' + correctGuesses);
+      // This currently doesn't handle repeated letters in activeWord properly!
+    }
+  }  
+  // If guess is incorrect, insert in incorrect letters array 
+    // Only log the incorrect guess if it doesn't already exist in incorrectGuesses
+      
 
-  // If guess is correct, push to correct letters array
-  if (activeWord.indexOf(letter) > -1) {
-    correctLetters.push(letter);
-    console.log('correct: ' + correctLetters);
-  }
- // If guess is incorrect, push to incorrect letters array 
-  else {
-    incorrectLetters.push(letter);
-    console.log('incorrect: ' + incorrectLetters);
-  }
-});
+}); // End main
 
 
