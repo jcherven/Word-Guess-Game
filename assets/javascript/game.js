8320cdce79b093d913c23390e7fc0ba7e4c207ad
James Cherven <jcherven@mail.usf.edu>
2018-09-05 02:41:18 -0400

    Adds game state handling. Game resets when win state or lose state is reached.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,9 @@
 // Global Variables
-/*
 const dictionary = [
     'hamburger', 'cheeseburger', 'fries', 'taco', 'nuggets',
     'doughnut', 'coffee', 'milkshake', 'soda', 'knife'
 ];
-*/
-const dictionary = ['bb'];
+//const dictionary = ['bb'];
 const letterBank = [
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
     'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
@@ -18,7 +16,6 @@ var playerGuessState = 0;
 var incorrectGuesses = [];
 // Current game state. 0 = gamplay loop is active. 1 = win state. 2 = lose state.
 var gameState = 0;
-document.getElementById('game-state').innerHTML = gameState;
 
 // Main Game Logic
 initGameEnvironment();
@@ -26,13 +23,10 @@ initGameEnvironment();
 document.onkeyup = function(event) {
     var playerInput = event.key.toLowerCase();
     playerGuessState = validateInput(playerInput, letterBank);
-    if ( playerGuessState === 1 ) {
-        // Invalid input
-        document.getElementById('feedback').innerHTML = "Letters only, please";
-    }
-    else
-        // Valid input
+    if ( playerGuessState === 0 )
         gameState = checkPlayerGuess(playerGuess);
+    if ( gameState === 1 || gameState === 2 )
+        initGameEnvironment();
 }; // End input capture
 // End main game logic
 
@@ -40,84 +34,16 @@ document.onkeyup = function(event) {
  *              Funtion Definitions
  ***************************************************/
 
-/**`checkPlayerGuess()`
- * Checks if the passed letter is in the activeWord, passes to insertCorrectGuess or insertIncorrectGuess.
- * Accepts:
- *      a letter
- * Returns:
- *      1 if in win state
- *      2 if in lose state
- */
-function checkPlayerGuess(letter) {
-    if (activeWordString.includes(letter) ) {
-        if ( insertCorrectGuess(letter) === 1 )
-            // Return win state
-            return 1;
-    }
-    else
-        if ( insertIncorrectGuess(letter) === 1 ) 
-            // Return lose state
-            return 2;
-        else
-            return 0;
-}
-
-/**`insertIncorrectGuess()`
- * Handles an incorrect guess and keeps track of the number of missed letters. Checks for lose state.
- * Accepts:
- *      a letter
- * Returns:
- *      1 if player is out of misses
- */
-function insertIncorrectGuess(incorrectletter) {
-    playerGuess = incorrectletter;
-    incorrectGuesses.push(incorrectletter);
-    incorrectGuesses.sort();
-    document.getElementById('misses').innerText = incorrectGuesses.length;
-    if (incorrectGuesses.length < 9 ) {
-        document.getElementById('missed-letters').innerHTML = incorrectGuesses.join(' ');
-        document.getElementById('alive-dead').innerHTML = "alive";
-        document.getElementById('game-state').innerHTML = gameState;
-        return 0;
-    }
-    else {
-        document.getElementById('missed-letters').innerHTML = incorrectGuesses.join(' ');
-        document.getElementById('alive-dead').innerHTML = "dead";
-        document.getElementById('game-state').innerHTML = gameState;
-        return 1;
-    }
-}
-
-/**`insertCorrectGuesses()`
- * Handles correct guesses. Checks for win state.
- *      a letter
- * Returns:
- *      1 if the word is complete (win state)
- */
-function insertCorrectGuess(letter) {
-    playerGuess = letter;
-    // compare letter to each item of activeWord, replace the same index of activeWordDisplay
-    for ( i=0; i < activeWordArray.length; i++ ) {
-        if ( activeWordArray[i] === letter ) {
-            activeWordDisplay[i] = letter;
-            document.getElementById('active-word-area').innerHTML = activeWordDisplay.join(' ');
-        }
-    }
-    if ( activeWordDisplay.includes('_') )
-        return 0;
-    else
-        return 1;
-}
-
-/* `initActiveWord()` 
- * Writes the currently active word to guess into the active-content-area element.
- * Returns an array of underscores for each letter.
+/* `initGameEnvironment()` 
+ * Writes the currently active word to guess into the active-content-area element. Sets all variables.
+ * Returns nothing.
  * Accepts:
  *     `word` (word to be replaced with underscores)
  *     `display` (array of underscores for each letter of `word`)
 */
 function initGameEnvironment() {
     activeWordString = pickWordFrom(dictionary);
+    console.log('activeWordString: ' + activeWordString)
     activeWordArray = activeWordString.split('');
     activeWordDisplay = [];
     playerGuess = null;
@@ -127,9 +53,10 @@ function initGameEnvironment() {
     for ( i=0; i < activeWordString.length; i++ ) 
         activeWordDisplay.push('_');
     document.getElementById('active-word-area').innerHTML = activeWordDisplay.join(' ');
-}
+    document.getElementById('missed-letters').innerHTML = incorrectGuesses.join(' ');
+} // End `initGameEnvironment()`
 
-/**`pickWordFrom()
+/**`pickWordFrom()`
  * Picks a word from the passed in array.
  * Returns:
  *      A word
@@ -138,7 +65,7 @@ function initGameEnvironment() {
  */
 function pickWordFrom(wordBank) {
     return wordBank[Math.floor(Math.random() * wordBank.length)];
-}
+} // End `pickWordFrom()`
 
 /**`validateInput()`
  * Checks player input against an array of valid characters.
@@ -156,7 +83,77 @@ function validateInput(keyboardInput, validList) {
     }
     else
         return 1;
-}
+} // End `validateInput()`
+
+/**`checkPlayerGuess()`
+ * Checks if the passed letter is in the activeWord, passes to insertCorrectGuess or insertIncorrectGuess. Sets the current game state.
+ * Accepts:
+ *      a letter
+ * Returns:
+ *      0 if win or loss state is not returned from insertIncorrectGuess or insertCorrectGuess.
+ *      1 if in win state
+ *      2 if in lose state
+ */
+function checkPlayerGuess(letter) {
+    if (activeWordString.includes(letter) ) {
+        if ( insertCorrectGuess(letter) === 1 )
+            // Return win state
+            return 1;
+    }
+    else
+        if ( insertIncorrectGuess(letter) === 1 ) 
+            // Return lose state
+            return 2;
+        else
+            return 0;
+} // End `checkPlayerGuess()`
+
+/**`insertIncorrectGuess()`
+ * Handles an incorrect guess from checkPlayerGuess and keeps track of the number of missed letters. Checks for lose state.
+ * Accepts:
+ *      a letter
+ * Returns:
+ *      1 if player is out of misses
+ */
+function insertIncorrectGuess(incorrectletter) {
+    playerGuess = incorrectletter;
+    incorrectGuesses.push(incorrectletter);
+    incorrectGuesses.sort();
+    document.getElementById('misses').innerText = incorrectGuesses.length;
+    if (incorrectGuesses.length < 9 ) {
+        document.getElementById('missed-letters').innerHTML = incorrectGuesses.join(' ');
+        return 0;
+    }
+    else {
+        document.getElementById('missed-letters').innerHTML = incorrectGuesses.join(' ');
+        return 1;
+    }
+} // End `insertIncorrectGuess()`
+
+/**`insertCorrectGuesses()`
+ * Handles a correct guess from checkPlayerGuess. Checks for win state.
+ *      a letter
+ * Returns:
+ *      1 if the word is complete (win state)
+ */
+function insertCorrectGuess(letter) {
+    playerGuess = letter;
+    // compare letter to each item of activeWord, replace the same index of activeWordDisplay
+    for ( i=0; i < activeWordArray.length; i++ ) {
+        if ( activeWordArray[i] === letter ) {
+            activeWordDisplay[i] = letter;
+            document.getElementById('active-word-area').innerHTML = activeWordDisplay.join(' ');
+        }
+    }
+    if ( activeWordDisplay.includes('_') )
+        return 0;
+    else
+        return 1;
+} // End `insertCorrectGuess()`
+
+
+
+
 
 
 
